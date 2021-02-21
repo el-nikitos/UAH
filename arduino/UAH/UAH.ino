2880c8b8ce4309ab0224ea47534cc90a68b4ddd0
AntonNikitin <kb.nikitinav@mail.ru>
2021-02-21 20:37:09 +0300

    исправления в прошивке

--- arduino/UAH/UAH.ino
+++ arduino/UAH/UAH.ino
@@ -18,8 +18,36 @@
 #define DIO_5           15
 #define DIO_6           14
 
+#define int_timer_overflow 100
+
+int int_timer_count = 0,
+    int_PWM_MOTOR_1 = 50,
+    int_PWM_MOTOR_2 = 30;
+
+int int_cicle_count = 0;
+
+int int_delay_1,
+    int_delay_2,
+    int_delay_3,
+    int_delay_4,
+    int_delay_5,
+
+    int_worltime_1,
+    int_worltime_2,
+    int_worltime_3,
+    int_worltime_4,
+    int_worltime_5,
+
+    int_PWM_fill_1,
+    int_PWM_fill_2,
+    int_PWM_fill_3,
+    int_PWM_fill_4,
+    int_PWM_fill_5,
+    
 
 void setup() {
+
+  Serial.begin(115200);
 
   pinMode(DIO_1, OUTPUT);
   digitalWrite(DIO_1, HIGH);
@@ -44,101 +72,66 @@ void setup() {
   pinMode(MOTOR_DRIVE, OUTPUT);
   pinMode(HEAT_DRIVE, OUTPUT);
 
+  delay(1000);
 }
 
 void loop() {
-  delay(1000);
 
-  //
-  digitalWrite(MOTOR_1_SPIN_2, LOW);  // мотор 1 - движение 1
-  delay(100);
-  digitalWrite(MOTOR_1_SPIN_1, HIGH);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 120);  
-  delay(2000);
-  analogWrite(MOTOR_DRIVE, 250);  
-  delay(2000);
+  digitalWrite(MOTOR_DRIVE, HIGH);
+
+  if (int_cicle_count < 500) {
+    programm_PWM( MOTOR_1_SPIN_1, int_timer_count, int_PWM_MOTOR_1 );
+  } 
+
+  if (int_cicle_count == 500)  {
+    turn_off_all();
+  }
+
+  if ( (int_cicle_count > 500) & (int_cicle_count < 1000) ) {
+    programm_PWM( MOTOR_1_SPIN_2, int_timer_count, int_PWM_MOTOR_2 );
+  }
+
+  if ( (int_cicle_count >= 1000) & (int_cicle_count < 1500) ) {
+    turn_off_all();
+  }
+  
+  if (int_cicle_count >= 1500)  {
+    int_cicle_count = 0;
+  }
+
+
+  delayMicroseconds( 100 ); 
+  
+  timer_counter();  // работа программного таймера ШИМ
+
+  //Serial.println(long_cicle_count);
+
+}
+
+void programm_PWM( int PWM_channel, int counter, int PWM_fill ) {
+  
+  if (counter <= PWM_fill)  { digitalWrite(PWM_channel, HIGH); }
+  else  { digitalWrite(PWM_channel, LOW); }
+
+}
+
+void turn_off_all() {
   digitalWrite(MOTOR_1_SPIN_1, LOW);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 0);  
-  delay(2000);
-
-  digitalWrite(MOTOR_1_SPIN_1, LOW);  // мотор 1 - движение 2
-  delay(100);
-  digitalWrite(MOTOR_1_SPIN_2, HIGH);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 120);  
-  delay(2000);
-  analogWrite(MOTOR_DRIVE, 250);  
-  delay(2000);
   digitalWrite(MOTOR_1_SPIN_2, LOW);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 0);  
-  delay(2000);
-  //
-  
-  //
-  digitalWrite(MOTOR_2_SPIN_2, LOW);  // мотор 2 - движение 1
-  delay(100);
-  digitalWrite(MOTOR_2_SPIN_1, HIGH);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 120);  
-  delay(2000);
-  analogWrite(MOTOR_DRIVE, 250);  
-  delay(2000);
   digitalWrite(MOTOR_2_SPIN_1, LOW);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 0);  
-  delay(2000);
-
-  digitalWrite(MOTOR_2_SPIN_1, LOW);  // мотор 2 - движение 2
-  delay(100);
-  digitalWrite(MOTOR_2_SPIN_2, HIGH);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 120);  
-  delay(2000);
-  analogWrite(MOTOR_DRIVE, 250);  
-  delay(2000);
   digitalWrite(MOTOR_2_SPIN_2, LOW);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 0);  
-  delay(2000);
-  //
-
-  //
-  digitalWrite(MOTOR_3_SPIN_2, LOW);  // мотор 3 - движение 1
-  delay(100);
-  digitalWrite(MOTOR_3_SPIN_1, HIGH);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 120);  
-  delay(2000);
-  analogWrite(MOTOR_DRIVE, 250);  
-  delay(2000);
   digitalWrite(MOTOR_3_SPIN_1, LOW);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 0);  
-  delay(2000);
-
-  digitalWrite(MOTOR_3_SPIN_1, LOW);  // мотор 3 - движение 2
-  delay(100);
-  digitalWrite(MOTOR_3_SPIN_2, HIGH);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 120);  
-  delay(2000);
-  analogWrite(MOTOR_DRIVE, 250);  
-  delay(2000);
   digitalWrite(MOTOR_3_SPIN_2, LOW);
-  delay(100);
-  analogWrite(MOTOR_DRIVE, 0);  
-  delay(2000);
-  //
+}
+
+void timer_counter()  {
   
-  analogWrite(HEAT_DRIVE, 120);
-  delay(2000);
-  analogWrite(HEAT_DRIVE, 250);
-  delay(2000);
-  analogWrite(HEAT_DRIVE, 0);
-  delay(2000);
+  int_timer_count++;
   
+  if (int_timer_count >= int_timer_overflow) {
+    
+    int_timer_count = 0;
+    int_cicle_count++;
+  }
 
 }
